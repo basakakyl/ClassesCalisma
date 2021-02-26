3e1444545418681657d37cb1739dbe5fc284f0da
ZELAL BAŞAK AKYOL <akyolzelalbasak@outlook.com>
2021-02-26 20:59:17 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Adana";
+            customer.Id = 1;
+            customer.FirstName = "Zelal";
+            customer.LastName = "Akyol";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City= "Niğde",FirstName="Başak", LastName="Akyol"
+            };
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+    
+
+    
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2] 
+            { 
+                new SqlServerCustomerDal(),
+                new OracleServerCustomerDal()
+            };
+
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Zelal", LastName = "Akyol", Address = "Adana" });
+
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Başak",
+                LastName = "Akyol",
+                Departmant = "Computer Engineering"
+
+            };
+            manager.Add(student);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer: IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
